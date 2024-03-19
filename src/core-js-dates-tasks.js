29589d442e544600b7ec1750847303517d1427c5
Andrey Kozimirov <an.kozimirov@gmail.com>
2024-03-19 14:37:58 +0300

    feat: implement getTime function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -34,8 +34,16 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const time = new Date(date);
+  const hour = time.getHours();
+  const minute = time.getMinutes();
+  const second = time.getSeconds();
+  let temp = '';
+  temp += (hour < 10 ? '0' : '') + hour;
+  temp += (minute < 10 ? ':0' : ':') + minute;
+  temp += (second < 10 ? ':0' : ':') + second;
+  return temp;
 }
 
 /**
